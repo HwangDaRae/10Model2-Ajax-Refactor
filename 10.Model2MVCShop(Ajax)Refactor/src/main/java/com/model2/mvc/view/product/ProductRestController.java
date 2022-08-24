bb8923a7b1c40e40816779ec7c5510583750acf8
903-19 <903-19@DESKTOP-4K59SEG>
2022-08-24 17:57:07 +0900

    new/dev : RestController 만듬 : 2022.08.24 05:57

---
+++ 10.Model2MVCShop(Ajax)Refactor/src/main/java/com/model2/mvc/view/product/ProductRestController.java
@@ -0,0 +1,70 @@
+package com.model2.mvc.view.product;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.StringTokenizer;
+import java.util.UUID;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.commons.fileupload.DiskFileUpload;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUpload;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.model2.mvc.common.Page;
+import com.model2.mvc.common.Search;
+import com.model2.mvc.common.util.CommonUtil;
+import com.model2.mvc.service.domain.Product;
+import com.model2.mvc.service.domain.Upload;
+import com.model2.mvc.service.domain.User;
+import com.model2.mvc.service.product.ProductService;
+import com.model2.mvc.service.product.impl.ProductServiceImpl;
+import com.model2.mvc.service.upload.UploadService;
+
+@RestController
+@RequestMapping("/product/*")
+public class ProductRestController {
+	
+	@Autowired
+	@Qualifier("productServiceImpl")
+	ProductService productServiceImpl;
+	
+	@Autowired
+	@Qualifier("uploadServiceImpl")
+	UploadService uploadServiceImpl;
+
+	public ProductRestController() {
+		System.out.println(getClass() + " default Constructor()]");
+		System.out.println("pageSize : " + pageSize);
+		System.out.println("pageUnit : " + pageUnit);
+	}
+	
+	@Value("#{commonProperties['pageUnit']}")
+	//@Value("#{commonProperties['pageUnit'] ?: 3}")
+	int pageUnit;
+	
+	@Value("#{commonProperties['pageSize']}")
+	//@Value("#{commonProperties['pageSize'] ?: 2}")
+	int pageSize;
+
+}
+
+
